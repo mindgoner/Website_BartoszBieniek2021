ec622ca16787c87f1ddea599dd414059e7622faf
Bartosz Bieniek <contact@bartoszbieniek.pl>
2021-06-12 00:08:52 +0200

    First commit.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bartosz Bieniek - portfolio</title>
+  <link type="text/css" rel="Stylesheet" href="css/default.css"/>
+</head>
+<body>
+    <iframe id="environment" src="pannellum/standalone/pannellum.htm?config=/map/map.json" frameBorder="0"></iframe>
+</body>
+</html>
